8922404a71d19210b4b63f94f0cd7cfb76e2fb30
Abhishek Kumar Jha <97616327+jhaabhiiishek@users.noreply.github.com>
2023-06-18 01:22:23 +0000

    Added images

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var image = new Image();
 function setImageBlogSize() {
-	image.src = './b.jpg';
+	image.src = './images/b.jpg';
 	var dynamicDiv = document.getElementById('blog');
   	dynamicDiv.style.backgroundImage = 'url("' + image.src + '")';
   	console.log(dynamicDiv.style.backgroundImage)
@@ -13,7 +13,7 @@ window.addEventListener('resize', setImageBlogSize);
 
 var image2 = new Image();
 function setImageBlog2Size() {
-	image2.src = './m.jpg';
+	image2.src = './images/m.jpg';
 	var dynamicDiv = document.getElementById('blog-2');
   	dynamicDiv.style.backgroundImage = 'url("' + image2.src + '")';
   	console.log(dynamicDiv.style.backgroundImage)
@@ -26,7 +26,7 @@ window.addEventListener('resize', setImageBlog2Size);
 
 var image3 = new Image();
 function setImageBlog3Size() {
-	image3.src = './d.jpg';
+	image3.src = './images/d.jpg';
 	var dynamicDiv = document.getElementById('blog-3');
   	dynamicDiv.style.backgroundImage = 'url("' + image3.src + '")';
   	console.log(dynamicDiv.style.backgroundImage)
@@ -39,7 +39,7 @@ window.addEventListener('resize', setImageBlog3Size);
 
 var image4 = new Image();
 function setImageBlog4Size() {
-	image4.src = './u.jpg';
+	image4.src = './images/u.jpg';
 	var dynamicDiv = document.getElementById('blog-4');
   	dynamicDiv.style.backgroundImage = 'url("' + image4.src + '")';
   	console.log(dynamicDiv.style.backgroundImage)
